239c9c58145a2016100107c377b1d1be5c2ca85d
Sam <samson.cannon@outlook.com>
2017-09-28 20:45:53 -0700

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let newCustomerWindow;
 
 app.on('ready', () => {
 
-	const mainWindow = new BrowserWindow({});
+	mainWindow = new BrowserWindow({
+		title: 'Solar Industries Quote'
+	});
 	
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+
+	Menu.setApplicationMenu(mainMenu);
+
 });
+
+const menuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Exit',
+				accelerator: (() => {
+					if(process.platform === 'darwin') {
+						return 'Command+Q';
+					} else {
+						return 'Ctrl+Q';
+					}
+				}) (),
+				click() {
+					app.quit();
+				}
+			}
+		]
+	}
+
+];
+
+let newCustomer = () => {
+
+	newCustomerWindow = new BrowserWindow({
+		width: 400,
+		height: 300,
+		title: 'Add New Customer'
+	});
+
+	newCustomerWindow.loadURL(`file://${__dirname}/index.html`);
+
+};
